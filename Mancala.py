0808715aaafe242a7ae441597e550e7e5b47ef66
Harma Everts <info@harmaeverts.nl>
2025-01-24 19:58:27 +0100

    Generated ui file.

--- Mancala.py
+++ Mancala.py
@@ -1,3 +1,13 @@
+from PySide6.QtWidgets import QApplication, QWidget
+# Only needed for access to command line arguments
+import sys
+
+# Important:
+# You need to run the following command to generate the ui_form.py file
+#     pyside6-uic form.ui -o ui_form.py, or
+#     pyside2-uic form.ui -o ui_form.py
+from ui_form import Ui_Mancala_UI
+
 from Board import Board
 from Player import Player
 
@@ -171,3 +181,17 @@ while not exit_game:
         exit_game = True
     else:
         print("That is not a valid option. Please choose 1, 2, or 3.")
+
+
+class Mancala_UI(QWidget):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.ui = Ui_Mancala_UI()
+        self.ui.setupUi(self)
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    widget = Mancala_UI()
+    widget.show()
+    sys.exit(app.exec())
