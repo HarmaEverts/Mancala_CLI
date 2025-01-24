f4e23d0ad96bf16acbec378152778ebb3a2718c9
Harma Everts <info@harmaeverts.nl>
2025-01-24 19:14:14 +0100

    Added comments.

--- Mancala.py
+++ Mancala.py
@@ -4,19 +4,20 @@ from Player import Player
 
 class Game:
     def __init__(self):
-        # Keep track of whether someone has won
+        # Keep track of whether someone has won.
         self.victory = False
 
-        # Create (default) players
+        # Create (default) players.
         self.player_a = Player("Player A", 6, 13, "A")
         self.player_b = Player("Player B", 13, 6, "B")
 
-        # Set the first turn to player A
+        # Set the first turn to player A.
         self.current_player = self.player_a
 
-        # Keep track of whether the player gets a free turn
+        # Keep track of whether the player gets a free turn.
         self.free_turn_earned = False
 
+        # Create the game board.
         self.board = Board()
 
     # Setting up the players
@@ -36,7 +37,7 @@ class Game:
 
     # Call this function to set the turn to the next player
     def change_turn(self):
-        if self.free_turn_earned: # Don't change turn!
+        if self.free_turn_earned:  # Don't change turn!
             return
         elif self.current_player == self.player_a:
             self.current_player = self.player_b
@@ -45,10 +46,8 @@ class Game:
         else:
             print("Something went horrible wrong... no current player selected.")
 
+    # Check if the winning condition has been met (one of the rows of pockets is empty)
     def check_for_victory(self):
-        # Add code here to check for winning condition and update the self.victory variable accordingly
-        # In this case, use a for loop to step through the first half of playing_area (0-5) and count if there are stones
-        # in there. Repeat for the other side (7-12). Don't count the stores in this.
         player_a_zone_count = 0
         player_b_zone_count = 0
         for pocket in range(0, 6):
@@ -67,12 +66,14 @@ class Game:
 
         return self.victory
 
+    # Play a turn and see if the player gets another turn or not.
     def play_turn(self):
         selection = self.choose_pocket()
         self.free_turn_earned = self.board.execute_move(selection, self.current_player)
         if self.free_turn_earned:
             print("Congratulations, you have earned a free turn, " + self.current_player.get_name() + "!")
 
+    # Choose which pocket to play
     def choose_pocket(self):
         valid_choice = False
         while not valid_choice:
@@ -80,6 +81,7 @@ class Game:
             valid_choice = self.board.is_pocket_choice_valid(chosen_pocket)
         return self.board.current_pocket
 
+    # Determine which player has won, whether it was a tie, and what the final scores are.
     def declare_winner(self):
         print("The game has finished!")
         if self.board.pockets[6].get_count() > self.board.pockets[13].get_count():
@@ -95,12 +97,15 @@ class Game:
               + self.player_b.get_name() + ": "
               + str(self.board.pockets[13].get_count()))
 
+    # Print the current score.
     def print_current_score(self):
         print("Current score: " + self.player_a.get_name() + ": "
               + str(self.board.pockets[6].get_count()) + " - "
               + self.player_b.get_name() + ": "
               + str(self.board.pockets[13].get_count()))
 
+
+# Manage the game loop
 def start_game():
     # Game loop
     game = Game()
@@ -122,23 +127,34 @@ def start_game():
     else:
         print("Thanks for playing!")
 
+
+# Display the game rules.
 def print_rules():
     print(
-        "------------------------------------------------------------------------------------------------------------------------------------")
+        "-------------------------------------------------------------------------------------------------------------"
+        "-----------------------")
     print("The Mancala 'board' is made up of two rows of six holes, or pits, each. "
-          "Next, four pieces -- marbles or stones -- are placed in each of the 12 holes. The color of the pieces is irrelevant.")
-    print("Each player has a 'store' to the side of the Mancala board. The number of stones in your store is your score.")
+          "Next, four pieces -- marbles or stones -- are placed in each of the 12 holes. "
+          "The color of the pieces is irrelevant.")
+    print(
+        "Each player has a 'store' to the side of the Mancala board. The number of stones in your store is your score.")
     print("The game begins with one player picking up all of the pieces in any one of the holes on his side. "
           "Moving counter-clockwise, the player deposits one of the stones in each hole until the stones run out.")
     print("1. If you run into your own store, deposit one piece in it. If you run into your opponent's store, skip it.")
     print("2. If the last piece you drop is in your own store, you get a free turn.")
-    print("3. If the last piece you drop is in an empty hole on your side, you capture that piece and any pieces in the hole directly opposite.")
+    print(
+        "3. If the last piece you drop is in an empty hole on your side, "
+        "you capture that piece and any pieces in the hole directly opposite.")
     print("4. Always place all captured pieces in your store.")
     print("The game ends when all six spaces on one side of the Mancala board are empty. "
           "The player who still has pieces on his side of the board when the game ends captures all of those pieces. "
           "Count all the pieces in each store. The winner is the player with the most pieces.")
-    print("------------------------------------------------------------------------------------------------------------------------------------")
+    print(
+        "-------------------------------------------------------------------------------------------------------------"
+        "-----------------------")
 
+
+# Kick off the game menu.
 print("Welcome to the Mancala game!")
 exit_game = False
 while not exit_game:
